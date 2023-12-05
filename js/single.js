9bbfdac67292401d32f8444fa53e52b06ee72639
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-05 11:19:45 +0200

    getSinglePost

---
+++ js/single.js
@@ -0,0 +1,8 @@
+'use strict';
+console.log('single.js file was loaded');
+
+const currentPostId = '3083126839342818';
+
+// su funkcija parsiusti ir iskonsolinti konretu posta kurio id yra currentPostId
+
+function getSinglePost() {}
