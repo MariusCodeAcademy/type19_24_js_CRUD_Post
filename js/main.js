60a3e686c133c54703423811cfcc44b2beaa5ef5
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-05 09:38:59 +0200

    getAllPosts

--- js/main.js
+++ js/main.js
@@ -5,5 +5,23 @@ const baseUrl = 'http://localhost:5000';
 const postsUrl = `${baseUrl}/posts`;
 
 // sukurti funkcija kuri parsiunia ir iskonsolina visus postus
+init();
+async function init() {
+  let mainPostsArr = await getAllPosts();
+  console.log('mainPostsArr ===', mainPostsArr);
+  makePostsHtml(mainPostsArr);
+}
+
+function getAllPosts() {
+  return fetch(postsUrl)
+    .then((resp) => resp.json())
+    .then((postsArr) => {
+      // console.log('postsArr ===', postsArr);
+      return postsArr;
+    })
+    .catch((error) => {
+      console.warn('ivyko klaida:', error);
+    });
+}
 
 // sugeneruoti postus htmle
