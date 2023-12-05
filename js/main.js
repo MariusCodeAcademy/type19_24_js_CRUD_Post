14f53b41302d1afe698023a6f80a020a1c5db828
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-05 09:07:10 +0200

    single post html, js start

---
+++ js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+console.log('main.js file was loaded');
+
+const baseUrl = 'http://localhost:5000';
+const postsUrl = `${baseUrl}/posts`;
+
+// sukurti funkcija kuri parsiunia ir iskonsolina visus postus
+
+// sugeneruoti postus htmle
