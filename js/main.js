ac461bb37e21ea7e14ccb732a949e80551aab4ae
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-05 10:22:47 +0200

    dynamic card

--- js/main.js
+++ js/main.js
@@ -44,13 +44,14 @@ function makePostsHtml(arr) {
 function createSinglePostEl(singlePostObj) {
   const liEl = document.createElement('li');
   const innerDiv = `
-    <div class="card" style="width: 18rem;">
+    <div class="card">
       <div class="card-body">
-        <h5 class="card-title">Post title</h5>
-        <h6 class="card-subtitle mb-2 text-body-secondary">Post author</h6>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-          content.</p>
-        <a href="#" class="btn btn-primary card-link">Read more</a>
+        <h5 class="card-title">${singlePostObj.title}</h5>
+        <h6 class="card-subtitle mb-2 text-body-secondary">${
+          singlePostObj.author
+        }</h6>
+        <p class="card-text">${singlePostObj.body.slice(0, 75)}...</p>
+        <a href="single-post.html" class="btn btn-primary card-link">Read more</a>
       </div>
     </div>
   `;
