6d2ca2518141e921f60ad47bded24c7b4dbc0079
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-05 11:51:09 +0200

    single page html

--- js/single.js
+++ js/single.js
@@ -1,8 +1,23 @@
 'use strict';
 console.log('single.js file was loaded');
 
+const baseUrl = 'http://localhost:5000';
 const currentPostId = '3083126839342818';
 
 // su funkcija parsiusti ir iskonsolinti konretu posta kurio id yra currentPostId
 
-function getSinglePost() {}
+function getSinglePost(url) {
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((postObj) => {
+      console.log('postObj ===', postObj);
+      fillHtmlPage(postObj);
+    })
+    .catch((error) => {
+      console.warn('ivyko klaida:', error);
+    });
+}
+
+getSinglePost(`${baseUrl}/posts/${currentPostId}`);
+
+function fillHtmlPage() {}
