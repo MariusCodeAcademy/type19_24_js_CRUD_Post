b7da0c7d2159c78548ec230df583820017fa4b61
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-05 09:55:54 +0200

    makePostsHtml, createSinglePostEl

--- js/main.js
+++ js/main.js
@@ -5,10 +5,16 @@ const baseUrl = 'http://localhost:5000';
 const postsUrl = `${baseUrl}/posts`;
 
 // sukurti funkcija kuri parsiunia ir iskonsolina visus postus
+const els = {
+  postListEl: document.getElementById('postList'),
+};
+console.log('els ===', els);
+
 init();
 async function init() {
   let mainPostsArr = await getAllPosts();
   console.log('mainPostsArr ===', mainPostsArr);
+  // console.log(JSON.stringify(mainPostsArr[0]));
   makePostsHtml(mainPostsArr);
 }
 
@@ -25,3 +31,29 @@ function getAllPosts() {
 }
 
 // sugeneruoti postus htmle
+function makePostsHtml(arr) {
+  arr.forEach((postObj) => {
+    // sukuriam viena post html el
+    const siglePostEl = createSinglePostEl(postObj);
+    // console.log('siglePostEl ===', siglePostEl);
+    // deti i sarasa
+    els.postListEl.append(siglePostEl);
+  });
+}
+
+function createSinglePostEl(singlePostObj) {
+  const liEl = document.createElement('li');
+  const innerDiv = `
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Post title</h5>
+        <h6 class="card-subtitle mb-2 text-body-secondary">Post author</h6>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+          content.</p>
+        <a href="#" class="btn btn-primary card-link">Read more</a>
+      </div>
+    </div>
+  `;
+  liEl.innerHTML = innerDiv;
+  return liEl;
+}
