0790410c66f99250ee3952febc43916411596af9
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-05 11:17:10 +0200

    delete works

--- js/main.js
+++ js/main.js
@@ -32,6 +32,8 @@ function getAllPosts() {
 
 // sugeneruoti postus htmle
 function makePostsHtml(arr) {
+  // isvalyti konteineri pries generuojant
+  els.postListEl.innerHTML = '';
   arr.forEach((postObj) => {
     // sukuriam viena post html el
     const siglePostEl = createSinglePostEl(postObj);
@@ -45,6 +47,9 @@ function createSinglePostEl(singlePostObj) {
   const liEl = document.createElement('li');
   const innerDiv = `
     <div class="card">
+      <div class="card-header">
+        id: ${singlePostObj.id}
+      </div>
       <div class="card-body">
         <h5 class="card-title">${singlePostObj.title}</h5>
         <h6 class="card-subtitle mb-2 text-body-secondary">${
@@ -56,5 +61,37 @@ function createSinglePostEl(singlePostObj) {
     </div>
   `;
   liEl.innerHTML = innerDiv;
+  // sukurti mygtuka delete
+  const deleteBtnEl = document.createElement('button');
+  deleteBtnEl.classList.add('btn', 'btn-outline-danger');
+  deleteBtnEl.textContent = 'Delete';
+  deleteBtnEl.addEventListener('click', () =>
+    sendDeleteFetch(singlePostObj.id)
+  );
+  liEl.querySelector('.card-body').append(deleteBtnEl);
+
   return liEl;
+}
+
+function sendDeleteFetch(idToDelete) {
+  console.log('deleting post', idToDelete);
+  // fetch su metodu delete
+  fetch(`${postsUrl}/${idToDelete}`, {
+    method: 'DELETE',
+  })
+    .then((resp) => {
+      console.log('resp ===', resp);
+      if (resp.status === 200) {
+        // istrinti pavyko
+        console.log('// istrinti pavyko');
+        // atnaujinam sarasa
+        init();
+      } else {
+        // istrinti nepavyko
+        console.log('// istrinti nepavyko');
+      }
+    })
+    .catch((error) => {
+      console.warn('ivyko klaida deleting:', error);
+    });
 }
