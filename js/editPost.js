3f671f93ffab9ffcd1453cfaf4addf7b8e683a7e
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-07 10:08:41 +0200

    uzd

--- js/editPost.js
+++ js/editPost.js
@@ -1,6 +1,15 @@
 'use strict';
 console.log('editPost.js file was loaded');
 
+const baseUrl = 'http://localhost:5000';
+let currentPostId = '5703187329379835';
+
+function flow() {
+  // eiga
+  // getSinglePost parsiustu posta ir grazintu
+  // supildom formos inputus
+}
+
 function getSinglePost(url) {
   fetch(url)
     .then((resp) => resp.json())
