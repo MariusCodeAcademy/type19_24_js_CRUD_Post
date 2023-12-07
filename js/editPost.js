8d7d7d3e903ee153d1528b1add282a1e616cb3a1
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-07 10:41:50 +0200

    ready to fillFormFields

--- js/editPost.js
+++ js/editPost.js
@@ -4,20 +4,40 @@ console.log('editPost.js file was loaded');
 const baseUrl = 'http://localhost:5000';
 let currentPostId = '5703187329379835';
 
+const els = {
+  form: document.forms[0],
+  image: document.getElementById('image'),
+  title: document.getElementById('title'),
+  body: document.getElementById('body'),
+  author: document.getElementById('author'),
+  tags: document.getElementById('tags'),
+  date: document.getElementById('date'),
+  errorList: document.getElementById('errorList'),
+  alert: document.getElementById('alert'),
+};
+console.log('els ===', els);
+
 flow();
-function flow() {
+async function flow() {
   // eiga
   getSetCurrentPostId();
+
   // getSinglePost parsiustu posta ir grazintu
+  const currentPostObj = await getSinglePost(
+    `${baseUrl}/posts/${currentPostId}`
+  );
+  console.log('currentPostObj ===', currentPostObj);
   // supildom formos inputus
+  fillFormFields(currentPostObj);
 }
 
 function getSinglePost(url) {
-  fetch(url)
+  return fetch(url)
     .then((resp) => resp.json())
     .then((postObj) => {
-      console.log('postObj ===', postObj);
-      fillHtmlPage(postObj);
+      // console.log('postObj ===', postObj);
+      // callback(postObj);
+      return postObj;
     })
     .catch((error) => {
       console.warn('ivyko klaida:', error);
@@ -35,3 +55,5 @@ function getSetCurrentPostId() {
     console.warn('Nera post id');
   }
 }
+
+function fillFormFields(postObj) {}
